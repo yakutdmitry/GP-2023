be351ba260eafd71f6a27c2d40ad94e20507e092
yakutdmitry <D23125246@mytudublin.ie>
2024-04-20 00:27:02 +0100

    comments added, small changes

--- Game Design Examples/soundboard.gd
+++ Game Design Examples/soundboard.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
- #volumes
+ #Chanel Volumes 
 
 func _on_v_slider_2_value_changed(value):
 	AudioServer.set_bus_volume_db(1, linear_to_db(value))
@@ -17,12 +17,6 @@ func _on_v_slider_5_value_changed(value):
 func _on_v_slider_6_value_changed(value):
 	AudioServer.set_bus_volume_db(5, linear_to_db(value))
 
-#Mutes
-
-
-
-
-		
 #Effects
 
 func _on_h_slider_4_value_changed(value): #pitch Shifter
@@ -37,21 +31,19 @@ func _on_h_slider_2_value_changed(value): #reverb
 	reverb.room_size = value
 
 
-func _on_h_slider_3_value_changed(value):
+func _on_h_slider_3_value_changed(value): #phazer
 	var phazer:AudioEffect = AudioServer.get_bus_effect(0, 2)
 	
 	phazer.range_max_hz = value
 
 
-func _on_h_slider_value_changed(value):
+func _on_h_slider_value_changed(value): # Amplify
 	var Amplify:AudioEffect = AudioServer.get_bus_effect(0, 3)
 	
 	Amplify.volume_db = value
 
 
-
-
-
+#Pause which doesn't work somehow
 
 func _on_pause_button_down():
 	$Vocals.get_stream_paused()
@@ -61,7 +53,7 @@ func _on_pause_button_down():
 	$Bass.get_stream_paused()
 
 
-
+#Play Button
 
 func _on_play_button_down():
 	$Mix/Vocals.play()
@@ -70,29 +62,22 @@ func _on_play_button_down():
 	$Mix/"Lead Guitar".play()
 	$Mix/Bass.play()
 
+# Chanel mutes
 
 func _on_mute_button_down():
 	AudioServer.set_bus_mute(0, !AudioServer.is_bus_mute(0))
 
-
 func _on_vocals_button_down():
 	AudioServer.set_bus_mute(1, !AudioServer.is_bus_mute(1))
-
 
 func _on_drums_button_down():
 	AudioServer.set_bus_mute(2, !AudioServer.is_bus_mute(2))
 
-
 func _on_bass_button_down():
 	AudioServer.set_bus_mute(3, !AudioServer.is_bus_mute(3))
 	
-
-
-
 func _on_g_1_button_down():
 	AudioServer.set_bus_mute(4, !AudioServer.is_bus_mute(4))
-
-
 
 func _on_g_2_button_down():
 	AudioServer.set_bus_mute(5, !AudioServer.is_bus_mute(5))
