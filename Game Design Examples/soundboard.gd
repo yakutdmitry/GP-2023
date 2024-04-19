8d833393469854ffc80b54b6d5d23193ccbfc408
yakutdmitry <D23125246@mytudublin.ie>
2024-04-19 22:41:52 +0100

    mutes added

--- Game Design Examples/soundboard.gd
+++ Game Design Examples/soundboard.gd
@@ -21,9 +21,20 @@ func _on_v_slider_6_value_changed(value):
 
 
 func _on_button_button_down():
-	AudioServer.set_bus_mute(0, !AudioServer.is_bus_mute(0))
+	AudioServer.set_bus_mute(1, !AudioServer.is_bus_mute(1))
 
-		
+
+func _on_button_2_button_down():
+	AudioServer.set_bus_mute(2, !AudioServer.is_bus_mute(2))
+	
+func _on_button_3_button_down():
+	AudioServer.set_bus_mute(3, !AudioServer.is_bus_mute(3))
+
+func _on_button_4_button_down():
+	AudioServer.set_bus_mute(4, !AudioServer.is_bus_mute(4))
+	
+func _on_button_5_button_down():
+	AudioServer.set_bus_mute(5, !AudioServer.is_bus_mute(5))	
 #Effects
 
 func _on_h_slider_4_value_changed(value): #pitch Shifter
@@ -38,13 +49,14 @@ func _on_h_slider_2_value_changed(value): #reverb
 	reverb.room_size = value
 
 
-func _on_h_slider_3_value_changed(value):
+func _on_h_slider_3_value_changed(value): #phazer
 	var phazer:AudioEffect = AudioServer.get_bus_effect(0, 2)
 	
 	phazer.range_max_hz = value
 
 
-func _on_h_slider_value_changed(value):
+func _on_h_slider_value_changed(value): #Amplify
 	var Amplify:AudioEffect = AudioServer.get_bus_effect(0, 3)
 	
 	Amplify.volume_db = value
+
