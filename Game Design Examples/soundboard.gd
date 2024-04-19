2e69bcb946929af6257ee19de3888df9e5ef0f7a
yakutdmitry <D23125246@mytudublin.ie>
2024-04-19 23:17:47 +0100

    corner buttons work

--- Game Design Examples/soundboard.gd
+++ Game Design Examples/soundboard.gd
@@ -21,20 +21,9 @@ func _on_v_slider_6_value_changed(value):
 
 
 func _on_button_button_down():
-	AudioServer.set_bus_mute(1, !AudioServer.is_bus_mute(1))
+	AudioServer.set_bus_mute(0, !AudioServer.is_bus_mute(0))
 
-
-func _on_button_2_button_down():
-	AudioServer.set_bus_mute(2, !AudioServer.is_bus_mute(2))
-	
-func _on_button_3_button_down():
-	AudioServer.set_bus_mute(3, !AudioServer.is_bus_mute(3))
-
-func _on_button_4_button_down():
-	AudioServer.set_bus_mute(4, !AudioServer.is_bus_mute(4))
-	
-func _on_button_5_button_down():
-	AudioServer.set_bus_mute(5, !AudioServer.is_bus_mute(5))	
+		
 #Effects
 
 func _on_h_slider_4_value_changed(value): #pitch Shifter
@@ -49,14 +38,39 @@ func _on_h_slider_2_value_changed(value): #reverb
 	reverb.room_size = value
 
 
-func _on_h_slider_3_value_changed(value): #phazer
+func _on_h_slider_3_value_changed(value):
 	var phazer:AudioEffect = AudioServer.get_bus_effect(0, 2)
 	
 	phazer.range_max_hz = value
 
 
-func _on_h_slider_value_changed(value): #Amplify
+func _on_h_slider_value_changed(value):
 	var Amplify:AudioEffect = AudioServer.get_bus_effect(0, 3)
 	
 	Amplify.volume_db = value
 
+
+
+
+
+
+func _on_pause_button_down():
+	$Vocals.get_stream_paused()
+	$Drums.get_stream_paused()
+	$"Rhytm Guitar".get_stream_paused()
+	$"Lead Guitar".get_stream_paused()
+	$Bass.get_stream_paused()
+
+
+
+
+func _on_play_button_down():
+	$Vocals.play()
+	$Drums.play()
+	$"Rhytm Guitar".play()
+	$"Lead Guitar".play()
+	$Bass.play()
+
+
+func _on_mute_button_down():
+	AudioServer.set_bus_mute(0, !AudioServer.is_bus_mute(0))
